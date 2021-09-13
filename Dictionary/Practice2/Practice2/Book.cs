f45cc7fd470f4d13f5a882a230fc286841c9079f
Mzolisi Dywili <mzolisi2304@gmail.com>
2021-09-13 22:35:03 +0200

    Copied Dictionary folder

---
+++ Dictionary/Practice2/Practice2/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice2
+{
+    public class Book
+    {
+        public string SerialNo { get; set; }
+        public string Name { get; set; }
+
+        public Book(string name, string isbn)
+        {
+            SerialNo = isbn;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return "ISBN: " + SerialNo + " Book Name: " + Name;
+        }
+    }
+}
